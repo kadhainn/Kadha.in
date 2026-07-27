f469160537aa1a8957ccfc3c838c4580e37f75b9
Kadha.in <shahishahhid@gmail.com>
2026-07-27 20:10:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-document.addEventListener("DOMContentLoaded",()=>{
+// kadha.in Script
 
-document.querySelectorAll(".card").forEach(card=>{
-card.onclick=()=>{
-alert(card.innerText+" വിഭാഗം ഉടൻ ലഭ്യമാകും.");
-};
+const search = document.querySelector(".hero input");
+
+search.addEventListener("keyup", function () {
+
+let value = this.value.toLowerCase();
+
+document.querySelectorAll(".story").forEach(story => {
+
+story.style.display =
+story.innerText.toLowerCase().includes(value)
+? "block"
+: "none";
+
 });
 
-document.querySelectorAll("button").forEach(button=>{
-button.onclick=()=>{
-alert("Coming Soon...");
+});
+
+document.querySelectorAll(".card").forEach(card=>{
+
+card.onclick=()=>{
+
+alert(card.querySelector("h3").innerText+" കഥകൾ ഉടൻ വരുന്നു ❤️");
+
 };
+
+});
+
+document.querySelectorAll("button").forEach(btn=>{
+
+btn.addEventListener("mouseenter",()=>{
+
+btn.style.transform="scale(1.05)";
+
+});
+
+btn.addEventListener("mouseleave",()=>{
+
+btn.style.transform="scale(1)";
+
 });
 
 });
